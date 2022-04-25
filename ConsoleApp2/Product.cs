ae8bd2f9d2b9cca906363bf1979e4421c985a002
Natsuftail <ufuk0demirel@hotmail.com>
2022-04-25 18:18:10 +0300

    product eklendi,

---
+++ ConsoleApp2/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+
+    //snippet (hazır kodlar ) prop--tab--tab 
+    class Product
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int UnitsInstock { get; set; }
+
+        // 4. video 21. dk
+
+    }
+}
